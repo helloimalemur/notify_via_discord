992472fec3af51f4b1555a123111f74524258bbc
foxx <jkoontsiii@gmail.com>
2023-01-15 14:40:03 -0500

    .

--- src/install_as_nagios_plugin.sh
+++ src/install_as_nagios_plugin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ -n /usr/local/nagios/libexec/ ]]; then
-  echo "dir '/usr/local/nagios/libexec/' does not exist";
+if [[ -f /usr/local/nagios/libexec/check_ping ]]; then
+  #  rm -rf /tmp/notify_via_discord/ 2>/dev/null;
+    cd /tmp/;
+    git clone https://github.com/helloimalemur/notify_via_discord.git;
+    cd notify_via_discord;
+    cargo build;
+    sudo cp target/debug/notify_via_discord /usr/local/nagios/libexec/;
 else
-#  rm -rf /tmp/notify_via_discord/ 2>/dev/null;
-  cd /tmp/;
-  git clone https://github.com/helloimalemur/notify_via_discord.git;
-  cd notify_via_discord;
-  cargo build;
-  sudo cp target/debug/notify_via_discord /usr/local/nagios/libexec/;
+  echo "dir '/usr/local/nagios/libexec/' does not exist";
 fi;
